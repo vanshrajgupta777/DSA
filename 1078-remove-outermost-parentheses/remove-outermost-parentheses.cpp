09faff28e45aff46cfe476b489357a21fd6f7595
Vanshraj Gupta <vanshrajgupta777@gmail.com>
2026-08-16 21:23:17 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (23.57%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -4,7 +4,7 @@ public:
         int n = s.length();
         int c=0;
         string ans = "";
-        int in = 1;
+        
         for(int i = 0; i<n;i++){
             
             if(s[i]=='(') {
